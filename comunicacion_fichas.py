d495941a021ddc846e6aa598ebd3ff1ec6c197d6
co-spe-ing <econtrerasb1@dian.gov.co>
2025-06-10 17:45:51 -0500

    Update comunicacion_fichas.py

--- comunicacion_fichas.py
+++ comunicacion_fichas.py
@@ -16,15 +16,6 @@ st.dataframe(df)
 
 
 
-#"""
-#sheet_name = 'Hoja1'
-#sheet_id = '1dyHiJaR3UySmG_7gQtamrDVfAqYFR_xW' 
-#url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-#r = requests.get(url)
-#data = r.content
-#df = pd.read_csv(BytesIO(data), index_col=0)
-#st.dataframe(df.head())
-#"""
 
 
 with st.form("my_form"):
