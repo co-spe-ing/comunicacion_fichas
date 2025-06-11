c883fed91d9ee62647f916c920cda270e9b3aef6
co-spe-ing <econtrerasb1@dian.gov.co>
2025-06-11 09:51:05 -0500

    Update comunicacion_fichas.py

--- comunicacion_fichas.py
+++ comunicacion_fichas.py
@@ -13,15 +13,7 @@ conn = st.connection("postgresql", type="sql")
 df = conn.query("SELECT version();", ttl="10m")
 st.dataframe(df)
 
-sql = """SELECT
-    table_schema || '.' || table_name
-FROM
-    information_schema.tables
-WHERE
-    table_type = 'BASE TABLE'
-AND
-    table_schema NOT IN ('pg_catalog', 'information_schema');
-"""
+sql = """SELECT * FROM pg_catalog.pg_tables;"""
 df = conn.query(sql, ttl="10m")
 st.dataframe(df)
 
