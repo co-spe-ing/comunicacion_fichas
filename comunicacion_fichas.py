14928d41df2db5f5493da8df9c890193570cefa5
co-spe-ing <econtrerasb1@dian.gov.co>
2025-06-11 10:37:27 -0500

    Update comunicacion_fichas.py

--- comunicacion_fichas.py
+++ comunicacion_fichas.py
@@ -7,11 +7,8 @@ import requests
 import psycopg2
 
 
-
 st.write("hola 11 de junio ...")
 
-
-# Connect to the database
 conn = psycopg2.connect(
     dbname = st.secrets.connections.postgresql.database,
     user = st.secrets.connections.postgresql.username,
@@ -19,35 +16,35 @@ conn = psycopg2.connect(
     host = st.secrets.connections.postgresql.host,
     port = st.secrets.connections.postgresql.port
 )
-
 cur = conn.cursor()
+
 cur.execute("SELECT version();")
 st.write(cur.fetchone())
+
+sql = """CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT,
+    department VARCHAR(50)
+);
+"""
+cur.execute(sql)
+
+sql = """INSERT INTO employees (name, age, department) VALUES
+    ('Alice Johnson', 30, 'Engineering'),
+    ('Bob Smith', 25, 'Marketing'),
+    ('Charlie Brown', 35, 'Sales');
+"""
+cur.execute(sql)
+
+sql = """SELECT * FROM employees;"""
+cur.execute(sql)
+st.write(cur.fetchone())
+
+
 cur.close()
 conn.close()
 
-
-#conn = st.connection("postgresql", type="sql")
-#df = conn.query("SELECT version();", ttl="10m")
-#st.dataframe(df)
-
-#sql = """CREATE TABLE employees (
-#    id SERIAL PRIMARY KEY,
-#    name VARCHAR(100) NOT NULL,
-#    age INT,
-#    department VARCHAR(50)
-#);
-#"""
-#conn.query(sql, ttl="10m")
-#ql = """INSERT INTO employees (name, age, department) VALUES
-#('Alice Johnson', 30, 'Engineering'),
-#('Bob Smith', 25, 'Marketing'),
-#('Charlie Brown', 35, 'Sales');
-#"""
-#conn.query(sql, ttl="10m")
-#sql = """SELECT * FROM employees;"""
-#df = conn.query(sql, ttl="10m")
-#st.dataframe(df)
 
 
 with st.form("my_form"):
