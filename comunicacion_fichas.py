407c84a714ab8f0aa4ad54fe61745137030d0e65
co-spe-ing <econtrerasb1@dian.gov.co>
2025-06-12 10:25:21 -0500

    Update comunicacion_fichas.py

--- comunicacion_fichas.py
+++ comunicacion_fichas.py
@@ -30,7 +30,7 @@ conn, cursor = nuevaConexion()
 cursor.execute("SELECT version();")
 st.write(cursor.fetchone())
 
-sql = """SELECT * FROM employees;"""
+sql = """SELECT * FROM personas;"""
 cursor.execute(sql)
 rows = cursor.fetchall()
 col_names = [desc[0] for desc in cursor.description]
@@ -40,7 +40,7 @@ st.dataframe(df)
 ###################################################################
 # CREAR Y POBLAR TABLAS
 ###################################################################
-crearYPoblarTablas = True
+crearYPoblarTablas = False
 if crearYPoblarTablas:
     ###################################################################
     # CREAR TABLAS
