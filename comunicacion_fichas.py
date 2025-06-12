9f20aafe57f223d8badb413a9d654c1e7f14a098
co-spe-ing <econtrerasb1@dian.gov.co>
2025-06-12 11:41:58 -0500

    Update comunicacion_fichas.py

--- comunicacion_fichas.py
+++ comunicacion_fichas.py
@@ -111,7 +111,7 @@ conn.close()
 
 with st.form("formulario_persona"):
     optCedula = st.selectbox('Cédula', personasdf["cedula"])
-    botonSeleccionPersona = st.form_submit_button("Seleccionar persona")
+    botonSeleccionPersona = st.form_submit_button("Buscar cédula")
     if botonSeleccionPersona:
         nombres = personasdf.loc[personasdf["cedula"]==optCedula, "nombres"].to_numpy()[0]
         apellidos = personasdf.loc[personasdf["cedula"]==optCedula, "apellidos"].to_numpy()[0]
@@ -122,15 +122,24 @@ with st.form("formulario_persona"):
         nivel4 = personasdf.loc[personasdf["cedula"]==optCedula, "nivel4"].to_numpy()[0]
         proceso = personasdf.loc[personasdf["cedula"]==optCedula, "proceso"].to_numpy()[0]
         subproceso = personasdf.loc[personasdf["cedula"]==optCedula, "subproceso"].to_numpy()[0]
-        st.write("**Nombres:**", nombres)
+        st.write("**Nombres:**")
+        st.code(nombres, language="markdown")
         st.write("**Apellidos:**", apellidos)
+        st.code(apellidos, language="markdown")
         st.write("**Cargo:**", cargo)
+        st.code(cargo, language="markdown")
         st.write("**Tipo de nombramiento:**", tiponombramiento)
+        st.code(tiponombramiento, language="markdown")
         st.write("**Dependencia nivel 2:**", nivel2)
+        st.code(nivel2, language="markdown")
         st.write("**Dependencia nivel 3:**", nivel3)
+        st.code(nivel3, language="markdown")
         st.write("**Dependencia nivel 4:**", nivel4)
+        st.code(nivel4, language="markdown")
         st.write("**Proceso:**", proceso)
+        st.code(proceso, language="markdown")
         st.write("**Subproceso:**", subproceso)
+        st.code(subproceso, language="markdown")
 
     
     slider_val = st.slider("Form slider")
