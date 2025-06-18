0d4714037253d7a4a2252c83ecbb172bd0741f3b
co-spe-ing <econtrerasb1@dian.gov.co>
2025-06-18 14:49:40 -0500

    Update comunicacion_fichas.py

--- comunicacion_fichas.py
+++ comunicacion_fichas.py
@@ -164,7 +164,7 @@ if (cedulaSeleccionada != None):
     
     ficha = st.selectbox(label="Ficha", options=fichasDelProcesoYCargo, index=None, placeholder="Selecciona una ficha...",)
     fechaFicha = st.date_input(label="Fecha de comunicación de la ficha", value="today", format="DD/MM/YYYY")
-    motivo = st.selectbox(label="Motivo del cambio de ficha (Opcional). Solo diligenciar si a alguien con ficha diligenciada se le cambia de nuevo la ficha.", options=("Reubicación","Cambio de funciones"), index=None, placeholder="Selecciona el motivo...", help="Solo diligenciar si a alguien con ficha se le cambia de nuevo la ficha")
+    motivo = st.selectbox(label="Motivo del cambio de ficha (Opcional). Solo diligenciar si a alguien con ficha diligenciada se le cambia de nuevo la ficha.", options=("Reubicación","Cambio de funciones"), index=None, placeholder="Selecciona el motivo...")
     observaciones = st.text_area(label="Observaciones (Opcional).", height=150)
 
     st.write("**¡Antes de guardar verifica que los datos han sido diligenciados correctamente!**")
